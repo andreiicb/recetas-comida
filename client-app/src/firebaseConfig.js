a4ff88dd40fd42c819435d0daddaed53c4c21aa7
Andrei Colio <andreicolio58@gmail.com>
2025-01-21 14:39:59 -0700

    ocultamos los secretos expuestos en el proyecto

--- client-app/src/firebaseConfig.js
+++ client-app/src/firebaseConfig.js
@@ -1,17 +1,18 @@
-// src/firebaseConfig.js
+// firebaseConfig.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-
+// Configuración pública de Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyBgZQ1SpB_ioQLb0X4zFGpfulR9EOpyYO0",
-    authDomain: "recetas-comidas.firebaseapp.com",
-    projectId: "recetas-comidas",
-    storageBucket: "recetas-comidas.firebasestorage.app",
-    messagingSenderId: "201413909427",
-    appId: "1:201413909427:web:c950cb1eaf31cec94ec9c6"
+  apiKey: "AIzaSyBgZQ1SpB_ioQLb0X4zFGpfulR9EOpyYO0",
+  authDomain: "recetas-comidas.firebaseapp.com",
+  projectId: "recetas-comidas",
+  storageBucket: "recetas-comidas.appspot.com",
+  messagingSenderId: "201413909427",
+  appId: "1:201413909427:web:c950cb1eaf31cec94ec9c6"
 };
 
+// Inicialización de Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
--- crud-app/firebaseAdminConfig.js
+++ crud-app/firebaseAdminConfig.js
@@ -1,16 +1,28 @@
-// firebaseAdminConfig.js
-
 import admin from 'firebase-admin';
-import { getFirestore } from 'firebase-admin/firestore';
-import fs from 'fs';
+import dotenv from 'dotenv';
 
-// Leer el archivo JSON con las credenciales
-const serviceAccount = JSON.parse(fs.readFileSync('./firebase-adminsdk.json', 'utf8'));
+// Cargar las variables del archivo .env
+dotenv.config();
 
+// Obtener las credenciales de las variables de entorno
+const serviceAccount = {
+  type: 'service_account',
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Reemplazar las secuencias de escape
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url: 'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-91lpd%40recetas-comidas.iam.gserviceaccount.com'
+};
+
+// Inicializar Firebase Admin con las credenciales del archivo .env
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
-const db = getFirestore();
+const db = admin.firestore();
 
 export { db };
